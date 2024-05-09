9969359c96d2bd6f0b70829411e6222aedf4ab58
Egor <104233247+RR3Z@users.noreply.github.com>
2024-05-09 19:14:52 +0300

    add: добавил пути для файлов

---
+++ src/main/java/model/utils/FilePaths.java
@@ -0,0 +1,6 @@
+package model.utils;
+
+public class FilePaths {
+    public final static String ALPHABET_FILE_PATH = "Data/alphabet.txt";
+    public final static String DICTIONARY_FILE_PATH = "Data/dictionary.txt";
+}
