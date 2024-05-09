e233c12f35f250f8fed33af8b137a7cec72ddb61
Egor <104233247+RR3Z@users.noreply.github.com>
2024-05-09 21:18:34 +0300

    add: выделил размеры и цвета для различных элементов в отдельный файл

---
+++ src/main/java/ui/utils/WidgetsViewCustomizations.java
@@ -0,0 +1,18 @@
+package ui.utils;
+
+import java.awt.*;
+
+public class WidgetsViewCustomizations {
+    public static final int CELL_BUTTON_SIZE = 50;
+    public static final int CELL_BUTTON_BORDER_THICKNESS = 1;
+    public static final Color STANDART_CELL_BUTTON_BORDER_COLOR = Color.BLACK;
+    public static final Color HOVERED_CELL_BUTTON_BORDER_COLOR = Color.ORANGE;
+    public static final Color CLICKED_CELL_BUTTON_COLOR = new Color(167, 201, 87);
+
+    public static final int KEYBOARD_ROW_COUNT = 3;
+    public static final int KEYBOARD_BUTTON_SIZE = 50;
+    public static final int KEYBOARD_BUTTON_BORDER_THICKNESS = 1;
+    public static final Color STANDART_KEYBOARD_BUTTON_BORDER_COLOR = Color.BLACK;
+    public static final Color HOVERED_KEYBOARD_BUTTON_BORDER_COLOR = Color.ORANGE;
+    public static final Color CLICKED_KEYBOARD_BUTTON_COLOR = new Color(167, 201, 87);
+}
