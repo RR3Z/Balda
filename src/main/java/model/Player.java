7932d9aed099976db676bf3052ec836732dfe619
Egor <104233247+RR3Z@users.noreply.github.com>
2024-05-10 16:06:29 +0300

    add: добавил getter имени для Player

--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -185,6 +185,8 @@ public class Player {
         return _state;
     }
 
+    public String name() { return _name; }
+
     /* ============================================================================================================== */
     // WordsDB observe
     private class WordsDBObserve implements WordsDBListener {
