5652f07a60dbdb0b213da37e4ec0c6f10a97336e
Egor <104233247+RR3Z@users.noreply.github.com>
2024-05-10 13:58:13 +0300

    add: добавил настройки для кнопок PlayerActionsWidget

--- src/main/java/ui/utils/WidgetsViewCustomizations.java
+++ src/main/java/ui/utils/WidgetsViewCustomizations.java
@@ -4,12 +4,14 @@ import javax.swing.*;
 import java.awt.*;
 
 public class WidgetsViewCustomizations {
-    public static final int CELL_BUTTON_SIZE = 50;
+    // CellButton view customizations
+    public static final int CELL_BUTTON_SIZE = 45;
     public static final int CELL_BUTTON_BORDER_THICKNESS = 1;
     public static final Color STANDART_CELL_BUTTON_BORDER_COLOR = Color.BLACK;
     public static final Color HOVERED_CELL_BUTTON_BORDER_COLOR = Color.ORANGE;
     public static final Color CLICKED_CELL_BUTTON_COLOR = new Color(167, 201, 87);
 
+    // KeyboardButton view customizations
     public static final int KEYBOARD_ROW_COUNT = 3;
     public static final int KEYBOARD_BUTTON_SIZE = 50;
     public static final int KEYBOARD_BUTTON_BORDER_THICKNESS = 1;
@@ -17,6 +19,7 @@ public class WidgetsViewCustomizations {
     public static final Color HOVERED_KEYBOARD_BUTTON_BORDER_COLOR = Color.ORANGE;
     public static final Color CLICKED_KEYBOARD_BUTTON_COLOR = new Color(167, 201, 87);
 
+    // For CellButton and KeyboardButton
     public static class FixedStateButtonModel extends DefaultButtonModel {
         @Override
         public boolean isPressed() {
@@ -32,4 +35,22 @@ public class WidgetsViewCustomizations {
         public void setRollover(boolean b) {
         }
     }
+
+
+    
+    // PlayerActionButton view customizations
+    public static final int PLAYER_ACTION_BUTTON_WIDTH = 130;
+    public static final int PLAYER_ACTION_BUTTON_HEIGHT = 40;
+    public static final int PLAYER_ACTION_BUTTON_BORDER_THICKNESS = 1;
+    public static final Color STANDART_PLAYER_ACTION_BUTTON_BORDER_COLOR = Color.BLACK;
+    public static final Color HOVERED_PLAYER_ACTION_BUTTON_BORDER_COLOR = Color.ORANGE;
+
+    // CancelActionButton view customizations
+    public static final Color CANCEL_ACTION_BUTTON_COLOR = new Color(167, 201, 87);
+
+    // SkipTurnButton view customizations
+    public static final Color SKIP_TURN_BUTTON_COLOR = new Color(167, 201, 87);
+
+    // SubmitWordButton view customizations
+    public static final Color SUBMIT_WORD_BUTTON_COLOR = new Color(167, 201, 87);
 }
