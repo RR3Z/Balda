5d8d3fef1eb458c29fa96359397a4bf83bd204fc
Egor <104233247+RR3Z@users.noreply.github.com>
2024-05-11 13:25:58 +0300

    add: вынес TableCellRenderer в TableUtils

---
+++ src/main/java/ui/utils/TableUtils.java
@@ -0,0 +1,18 @@
+package ui.utils;
+
+import javax.swing.table.DefaultTableCellRenderer;
+import java.awt.*;
+
+public class TableUtils {
+    public static DefaultTableCellRenderer CUSTOM_TABLE_CELL_RENDERER = new DefaultTableCellRenderer() {
+        @Override
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.setColor(Color.BLACK); // Цвет линий
+            g.fillRect(0, 0, 1, getHeight()); // Левая вертикальная линия
+            g.fillRect(0, 0, getWidth(), 1); // Верхняя горизонтальная линия
+            g.fillRect(getWidth() - 1, 0, 1, getHeight()); // Правая вертикальная линия
+            g.fillRect(0, getHeight() - 1, getWidth(), 1); // Нижняя горизонтальная линия
+        }
+    };
+}
