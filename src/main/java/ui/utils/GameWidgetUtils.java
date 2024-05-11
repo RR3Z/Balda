3ae435f50703e6e3d1d2b7da4dd606fc2687a12e
Egor <104233247+RR3Z@users.noreply.github.com>
2024-05-11 22:43:32 +0300

    add: поменял цвета и добавил метод getBorderThickness

--- src/main/java/ui/utils/GameWidgetUtils.java
+++ src/main/java/ui/utils/GameWidgetUtils.java
@@ -1,11 +1,11 @@
 package ui.utils;
 
+import ui.enums.BorderType;
 import ui.enums.ColorType;
 
 import java.awt.*;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Map;
 
@@ -29,16 +29,20 @@ public class GameWidgetUtils {
             case TRANSPARENT -> new Color(0, 0, 0, 0);
             case BACKGROUND -> new Color(250, 237,205, 80);
             case DEFAULT_BORDER -> Color.BLACK;
-            case HOVERED_BORDER -> Color.ORANGE;
-            case CELL_IN_WORD -> new Color(167, 201, 87);
-            case CHANGED_CELL -> new Color(235, 93, 95);
-            case CANCEL_ACTION_BUTTON -> new Color(167, 201, 87);
-            case SKIP_TURN_BUTTON -> new Color(235, 93, 95);
-            case SUBMIT_WORD_BUTTON -> new Color(167, 201, 87);
-            case ACTIVE_KEYBOARD_BUTTON -> new Color(167, 201, 87);
+            case DEFAULT_HIGHLIGHTED_BORDER -> new Color(98, 138, 5);
+            case HIGHLIGHTED_SKIP_TURN_BORDER -> new Color(148, 15, 17);
+            case CELL_IN_WORD, CANCEL_ACTION_BUTTON, ACTIVE_KEYBOARD_BUTTON, SUBMIT_WORD_BUTTON, ACTIVE_PLAYER -> new Color(167, 201, 87);
+            case CHANGED_CELL, SKIP_TURN_BUTTON -> new Color(235, 93, 95);
             case TABLE_HEADER -> new Color(242, 232, 207);
-            case ACTIVE_PLAYER -> new Color(167, 201, 87);
             default -> Color.RED;
+        };
+    }
+
+    public static int getBorderThickness(BorderType type) {
+        return switch (type) {
+            case DEFAULT -> 1;
+            case BOLD -> 2;
+            default -> 1;
         };
     }
 
