5f28f91da19c3868f730f6e514c410e092249389
Egor <104233247+RR3Z@users.noreply.github.com>
2024-05-13 17:25:16 +0300

    update: заменил PLAIN_MESSAGE на INFORMATION_MESSAGE в MainWindow

--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -234,8 +234,9 @@ public class MainWindow extends JFrame {
         @Override
         public void addedNewWordToDictionary(WordsDBEvent event) {
             if(event.player() == _gameModel.activePlayer()) {
-                String message = "<html><div style='text-align: center;'>" + GameWidgetUtils.htmlFont(GameWidgetUtils.OPTION_PANE_FONT_SIZE) + "Слово \"" + event.word() + "\" успешно добавлено" + "</div></html>";
-                JOptionPane.showMessageDialog(null, message, "Новое слово", JOptionPane.PLAIN_MESSAGE);
+                String message = "<html><div style='text-align:center;'>" + GameWidgetUtils.htmlFont(GameWidgetUtils.OPTION_PANE_FONT_SIZE) +
+                        "Слово \"" + event.word() + "\" успешно добавлено" + "</div></html>";
+                JOptionPane.showMessageDialog(null, message, "Новое слово", JOptionPane.INFORMATION_MESSAGE);
                 _gameModel.activePlayer().submitWord();
             }
         }
