c0d945b793156c77c9e8fcc6c46d1dd5ed94b91b
Egor <104233247+RR3Z@users.noreply.github.com>
2024-05-28 14:24:18 +0300

    add: вынес значения для Spinner в отдельный файл SpinnerConfig

--- src/main/java/ui/panels/GameSettingsPanel.java
+++ src/main/java/ui/panels/GameSettingsPanel.java
@@ -1,6 +1,7 @@
 package ui.panels;
 
 import ui.utils.GameWidgetUtils;
+import ui.utils.configs.SpinnerConfig;
 
 import javax.swing.*;
 
@@ -14,8 +15,8 @@ public class GameSettingsPanel extends JPanel {
 
         JLabel fieldSizesLabel = new JLabel("Выберите размеры поля:");
 
-        _widthSpinner = new JSpinner(new SpinnerNumberModel(5, GameWidgetUtils.MIN_SPINNER_VALUE_FOR_FIELD_SIZE, GameWidgetUtils.MAX_SPINNER_VALUE_FOR_FIELD_SIZE, 1));
-        _heightSpinner = new JSpinner(new SpinnerNumberModel(5, GameWidgetUtils.MIN_SPINNER_VALUE_FOR_FIELD_SIZE, GameWidgetUtils.MAX_SPINNER_VALUE_FOR_FIELD_SIZE, 1));
+        _widthSpinner = new JSpinner(new SpinnerNumberModel(5, SpinnerConfig.MIN_SPINNER_VALUE_FOR_FIELD_SIZE, SpinnerConfig.MAX_SPINNER_VALUE_FOR_FIELD_SIZE, 1));
+        _heightSpinner = new JSpinner(new SpinnerNumberModel(5, SpinnerConfig.MIN_SPINNER_VALUE_FOR_FIELD_SIZE, SpinnerConfig.MAX_SPINNER_VALUE_FOR_FIELD_SIZE, 1));
 
         fieldSizesPanel.add(fieldSizesLabel);
         fieldSizesPanel.add(_widthSpinner);
---
+++ src/main/java/ui/utils/configs/SpinnerConfig.java
@@ -0,0 +1,6 @@
+package ui.utils.configs;
+
+public class SpinnerConfig {
+    public final static int MIN_SPINNER_VALUE_FOR_FIELD_SIZE = 2;
+    public final static int MAX_SPINNER_VALUE_FOR_FIELD_SIZE = 14;
+}
