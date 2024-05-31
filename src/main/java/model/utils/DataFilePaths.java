72b6ab1c617f95b263b107e67b3f39401b69b909
Chernobaev Egor <104233247+RR3Z@users.noreply.github.com>
2024-05-31 12:54:12 +0300

    add: добавил файл для словаря для тестирования

--- src/main/java/model/utils/DataFilePaths.java
+++ src/main/java/model/utils/DataFilePaths.java
@@ -3,4 +3,5 @@ package model.utils;
 public class DataFilePaths {
     public final static String ALPHABET_FILE_PATH = "Data/alphabet.txt";
     public final static String DICTIONARY_FILE_PATH = "Data/dictionary.txt";
+    public final static String DICTIONARY_FILE_PATH_FOR_TESTS = "Data/tests_dictionary.txt";
 }
